86dc48a083db8ac1bdcf42b84bf01bc5b4854fd9
aaron235 <qwertyman159@gmail.com>
2016-01-08 00:12:50 -0800

    added some basic desktop rejection

--- scripts/main.js
+++ scripts/main.js
@@ -35,5 +35,9 @@ if ( navigator.vibrate ) {
 
 } else {
 	//  fade in the no-vibe div
-	var s = document.getElementById('thing').style; s.opacity = 1; (function fade(){(s.opacity-=.1)<0?s.display="none":setTimeout(fade,40)})();
+	console.log( "THIS DOESN'T HAVE A VIBRATOR FUCK YOU" );
+	var elStyle = document.getElementById( 'desktopWarning' ).style;
+	elStyle.display = '';
+	//  elStyle.opacity = 0;
+
 }
