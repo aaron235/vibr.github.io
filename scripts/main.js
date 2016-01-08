e1f6c1ca3d9012a99b994f86cedb400a16818243
aaron235 <qwertyman159@gmail.com>
2016-01-08 02:22:52 -0800

    Did some fine tuning

--- scripts/main.js
+++ scripts/main.js
@@ -1,35 +1,38 @@
 //  the duty cycle period, in ms:
-var DUTY_CYCLE = 50;
+var DUTY_CYCLE = 30;
 
 //  Here, we set the navigator's vibrate object so that it is consistent:
 navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
 
-navigator.vibrate( 1000 );
-
 if ( navigator.vibrate ) {
 	//  we're on a good platform!
-
+	navigator.vibrate( [250, 100, 100, 70, 500] );
 	//  vibe the vibes. accepts a power level from 0 to 100.
 	vibe = function( power ) {
 		if ( power < 0 || power > 100 ) {
-			console.log( "value out of range" );
-		} else if ( power < 10 ) {
-			power = 10;
+			console.log( "fuck you" );
+		} else if ( power === 0 ) {
+			killTheVibes();
+		} else {
+			power = power * 1.1;
+			power = Math.max( 25, power );
+			power = Math.min( 100, power );
+
+			var onTime  = ( power / 100 ) * DUTY_CYCLE;
+			var offTime = DUTY_CYCLE - onTime;
+			navigator.vibrate( [onTime, offTime] );
 		}
-
-		var onTime  = ( power / 100 ) * DUTY_CYCLE;
-		navigator.vibrate( onTime );
-
 	};
 
 	//  set a vibe level to repeat at.
 	window.setVibes = function( power ) {
-		window.vibes = setInterval( function() { vibe( power ) }, DUTY_CYCLE );
+		window.killTheVibes();
+		window.vibes = window.setInterval( function() { vibe( power ) }, DUTY_CYCLE );
 	};
 
 	//  stop vibing.
 	window.killTheVibes = function() {
-		clearInterval( window.vibes );
+		window.clearInterval( window.vibes );
 	};
 
 	//  start vibing at 0 power:
@@ -37,6 +40,7 @@ if ( navigator.vibrate ) {
 
 } else {
 	//  fade in the no-vibe div
+	console.log( "THIS DOESN'T HAVE A VIBRATOR FUCK YOU" );
 	var elStyle = document.getElementById( 'desktopWarning' ).style;
 	elStyle.display = '';
 	//  elStyle.opacity = 0;
