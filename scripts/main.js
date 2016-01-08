dacecdae4bdeb3c7aa2518bbf8e686b73963a37c
aaron235 <qwertyman159@gmail.com>
2016-01-07 21:23:45 -0800

    Fixed up the missing semicolons and `var` statements

--- scripts/main.js
+++ scripts/main.js
@@ -8,11 +8,11 @@ if ( navigator.vibrate ) {
 	//  we're on a good platform!
 
 	//  vibe the vibes. accepts a power level from 0 to 100.
-	function vibe( power ) {
-		onTime  = ( power / 100 ) * DUTY_CYCLE
-		offtime = DUTY_CYCLE - onTime
+	vibe = function( power ) {
+		var onTime  = ( power / 100 ) * DUTY_CYCLE;
+		var offtime = DUTY_CYCLE - onTime;
 		navigator.vibrate( [onTime, offTime] );
-	}
+	};
 	//  start "vibrating" at 0 power:
 	window.vibes = setInterval( vibe( 0 ), DUTY_CYCLE );
 
