1f01f744157d285df9be7f94e4a6f6fc578a8f7a
aaron235 <qwertyman159@gmail.com>
2016-01-07 21:22:40 -0800

    Added a function to vibe and ran it with 0 power at the start

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,23 @@
+//  the duty cycle period, in ms:
+var DUTY_CYCLE = 50;
+
 //  Here, we set the navigator's vibrate object so that it is consistent:
 navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
 
 if ( navigator.vibrate ) {
 	//  we're on a good platform!
+
+	//  vibe the vibes. accepts a power level from 0 to 100.
+	function vibe( power ) {
+		onTime  = ( power / 100 ) * DUTY_CYCLE
+		offtime = DUTY_CYCLE - onTime
+		navigator.vibrate( [onTime, offTime] );
+	}
+	//  start "vibrating" at 0 power:
+	window.vibes = setInterval( vibe( 0 ), DUTY_CYCLE );
+
+
+
 } else {
 	alert( "you can not vibrate" );
 }
