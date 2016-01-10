6a198b1c141418383dd746550d51f819aec36a8e
aaron235 <qwertyman159@gmail.com>
2016-01-09 22:41:49 -0800

    Added prevention of dragging on desktop

--- scripts/ui.js
+++ scripts/ui.js
@@ -1,57 +1,59 @@
-window.onload = function () {
-	var selected = null;
-	var sliderArea = document.querySelector("body");
-	var slider = document.querySelector("#slider");
-	var zeroZone = document.querySelector("header");
-	sliderArea.addEventListener("touchstart", function (event) {
-		init(event);
-	}, false);
-	sliderArea.addEventListener("mousedown", function (event) {
-		init(event);
-	}, false);
-	sliderArea.addEventListener("touchmove", function (event) {
-		move(event);
-	}, false);
-	sliderArea.addEventListener("mousemove", function (event) {
-		move(event);
-	}, false);
-	sliderArea.addEventListener("touchend", function (event) {
-		end(event);
-	}, false);
-	sliderArea.addEventListener("mouseup", function (event) {
-		end(event);
-	}, false);
+if ( navigator.vibrate ) {
+	window.onload = function () {
+		var selected = null;
+		var sliderArea = document.querySelector("body");
+		var slider = document.querySelector("#slider");
+		var zeroZone = document.querySelector("header");
+		sliderArea.addEventListener("touchstart", function (event) {
+			init(event);
+		}, false);
+		sliderArea.addEventListener("mousedown", function (event) {
+			init(event);
+		}, false);
+		sliderArea.addEventListener("touchmove", function (event) {
+			move(event);
+		}, false);
+		sliderArea.addEventListener("mousemove", function (event) {
+			move(event);
+		}, false);
+		sliderArea.addEventListener("touchend", function (event) {
+			end(event);
+		}, false);
+		sliderArea.addEventListener("mouseup", function (event) {
+			end(event);
+		}, false);
 
-	function init(event) {
-		event.preventDefault();
-		selected = true;
-	}
+		function init(event) {
+			event.preventDefault();
+			selected = true;
+		}
 
-	function move(event) {
-		event.preventDefault();
-		if (selected) {
-			x_pos = event.clientX || event.touches[0].pageX;
-			y_pos = event.clientY || event.touches[0].pageY;
-			var totalWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+		function move(event) {
+			event.preventDefault();
+			if (selected) {
+				x_pos = event.clientX || event.touches[0].pageX;
+				y_pos = event.clientY || event.touches[0].pageY;
+				var totalWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
 
-			var totalHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-			var heightOffset = zeroZone.offsetHeight;
-			var height = totalHeight - heightOffset;
-			var percentage = Math.round(Math.max(height - y_pos, 0) / (heightOffset + height) * 100);
-			var blah = document.getElementById("desktopWarning");
-			blah.innerHTML = "percentage: " + percentage + "%";
-			slider.style.bottom = percentage + "%";
-			window.setVibes(percentage);
+				var totalHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+				var heightOffset = zeroZone.offsetHeight;
+				var height = totalHeight - heightOffset;
+				var percentage = Math.round(Math.max(height - y_pos, 0) / (heightOffset + height) * 100);
+				var blah = document.getElementById("desktopWarning");
+				blah.innerHTML = "percentage: " + percentage + "%";
+				slider.style.bottom = percentage + "%";
+				window.setVibes(percentage);
 
-			//Check if we're getting to the bounds of the screen and stop tracking
-			if (y_pos < 10 || y_pos > totalHeight - 10 || x_pos < 10 | x_pos > totalWidth - 10) {
-				selected = false;
+				//Check if we're getting to the bounds of the screen and stop tracking
+				if (y_pos < 10 || y_pos > totalHeight - 10 || x_pos < 10 | x_pos > totalWidth - 10) {
+					selected = false;
+				}
 			}
 		}
-	}
 
-	function end(event) {
-		event.preventDefault();
-		selected = false;
+		function end(event) {
+			event.preventDefault();
+			selected = false;
+		}
 	}
 }
